71f6ac384096c240d3a9ce10db5642a64e129db7
ElisaBerisha <154630631+ElisaBerisha@users.noreply.github.com>
2025-06-19 09:58:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Colors
+
 RED = "\033[91m"
 GREEN = "\033[92m"
 YELLOW = "\033[93m"
 CYAN = "\033[96m"
 RESET = "\033[0m"
+LIGHT_PURPLE = "\033[38;2;224;170;255m"  
+DARK_PURPLE = "\033[38;2;60;9;108m"
 
 # API Keys
 VIRUSTOTAL_API_KEY = os.getenv("VIRUSTOTAL_API_KEY")
@@ -23,6 +25,5 @@ if not VIRUSTOTAL_API_KEY or not SHODAN_API_KEY:
     print(f"{RED}[!] API keys not found. Set them in .env file{RESET}")
     sys.exit(1)
 
-# Output configs
 OUTPUT_FOLDER = "output"
 VULNERABLE_FILE = "vulnerable.txt"
